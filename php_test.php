3b3eba87958a10b39b7e1f538d0e61452c272aa0
Winizsol <szabo.zsolo.laszlo@gmail.com>
2017-06-26 18:39:00 +0200

    LAMP tesztelési eszközök feltöltve
    apache_test.html feltöltve az Apache2 teszteléséhez
    php_test.php feltöltve a PHP teszteléséhez
    style.css stílusfájl feltöltve a php_test.php-hez

---
+++ php_test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <head>
+      <title>
+        Telepitesi Scriptek
+      </title>
+      <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <h1>
+      PHP Informaciok
+    </h1>
+    <div class="warning">
+      <p>
+        Amennyiben latja lent a PHP Informaciokat, AZONNAL TOROLJE KI A FAJLT NYILVANOS SZERVEREN BIZTONSAGI OKOKBOL!
+      </p>
+    </div>
+    <p>
+      <b>Miert okoz biztonsagi problemakat?</b> - Mert a tamadok lathatjak a PHP verziojat, az operacios rendszert es akar annak a verziojat, amin fut a szerver es ha nem volt frissitve, kihasznalhatja a regebbi verziokban felfedezett hibakat, amik az ujabbakban mar ki lettek javitva.
+    </p>
+    <?php
+    phpinfo();
+    ?>
+  </body>
+</html>
